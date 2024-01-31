27ddea5074f5e6e0f9f615c4a594ca35296c24b3
Joseph Eiba <josepheiba@icloud.com>
2024-01-31 22:36:04 +0100

    ok

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,77 +1,78 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { "catppuccin/nvim",                  name = "catppuccin", priority = 1000 },
-  { "rebelot/kanagawa.nvim",            name = "kanagawa",   priority = 1000 },
-  { "williamboman/mason.nvim" },
-  { "williamboman/mason-lspconfig.nvim" },
-  -- LSP Support
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v3.x",
-    lazy = true,
-    config = false,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      { "hrsh7th/cmp-nvim-lsp" },
-    },
-  },
-  -- Autocompletion
-  { "hrsh7th/nvim-cmp" },
-  { "L3MON4D3/LuaSnip" },
-  { "rafamadriz/friendly-snippets" },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-  },
-  "nvim-tree/nvim-web-devicons",
-  { "echasnovski/mini.nvim", version = "*" },
-  "lewis6991/gitsigns.nvim",
-  "nvim-tree/nvim-tree.lua",
-  -- Test
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/cmp-path",
-  "hrsh7th/cmp-cmdline",
-  "hrsh7th/nvim-cmp",
-  "saadparwaiz1/cmp_luasnip",
-  "onsails/lspkind.nvim",
-  {
-    "numToStr/Comment.nvim",
-    opts = {
-      -- add any options here
-    },
-    lazy = false,
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = {},
-  },
-  "RRethy/vim-illuminate",
-  {
-    "stevearc/conform.nvim",
-    opts = {},
-  },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ "rebelot/kanagawa.nvim", name = "kanagawa", priority = 1000 },
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	-- LSP Support
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		lazy = true,
+		config = false,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{ "hrsh7th/cmp-nvim-lsp" },
+		},
+	},
+	-- Autocompletion
+	{ "hrsh7th/nvim-cmp" },
+	{ "L3MON4D3/LuaSnip" },
+	{ "rafamadriz/friendly-snippets" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+	"nvim-tree/nvim-web-devicons",
+	{ "echasnovski/mini.nvim", version = "*" },
+	"lewis6991/gitsigns.nvim",
+	"nvim-tree/nvim-tree.lua",
+	-- Test
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-cmdline",
+	"hrsh7th/nvim-cmp",
+	"saadparwaiz1/cmp_luasnip",
+	"onsails/lspkind.nvim",
+	{
+		"numToStr/Comment.nvim",
+		opts = {
+			-- add any options here
+		},
+		lazy = false,
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+	},
+	"RRethy/vim-illuminate",
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+	},
+	"mfussenegger/nvim-lint",
 })
 
 require("nvim-treesitter.configs").setup({
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+	auto_install = true,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
 })
 
 vim.g.lsp_zero_ui_float_border = 0
@@ -88,27 +89,26 @@ require("nvim-tree").setup()
 local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({ buffer = bufnr })
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 lsp_zero.set_sign_icons({
-  error = "",
-  warn = "",
-  hint = "",
-  info = "",
+	error = "",
+	warn = "",
+	hint = "",
+	info = "",
 })
 
 --- if you want to know more about lsp-zero and mason.nvim
 --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require("mason").setup({})
 require("mason-lspconfig").setup({
-  handlers = {
-    lsp_zero.default_setup,
-  },
+	handlers = {
+		lsp_zero.default_setup,
+	},
 })
-
 
 local luasnip = require("luasnip")
 local cmp = require("cmp")
@@ -118,111 +118,120 @@ local cmp_format = require("lsp-zero").cmp_format()
 require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "buffer" },
-  },
-  mapping = cmp.mapping.preset.insert({
-    -- `Enter` key to confirm completion
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+	},
+	mapping = cmp.mapping.preset.insert({
+		-- `Enter` key to confirm completion
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
 
-    -- Ctrl+Space to trigger completion menu
-    ["<C-Space>"] = cmp.mapping.complete(),
+		-- Ctrl+Space to trigger completion menu
+		["<C-Space>"] = cmp.mapping.complete(),
 
-    -- Navigate between snippet placeholder
-    ["<C-f>"] = cmp_action.luasnip_jump_forward(),
-    ["<C-b>"] = cmp_action.luasnip_jump_backward(),
+		-- Navigate between snippet placeholder
+		["<C-f>"] = cmp_action.luasnip_jump_forward(),
+		["<C-b>"] = cmp_action.luasnip_jump_backward(),
 
-    -- Scroll up and down in the completion documentation
-    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-d>"] = cmp.mapping.scroll_docs(4),
-    -- Super Tab
-    ["<Tab>"] = cmp_action.luasnip_supertab(),
-    ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
-  }),
-  --- (Optional) Show source name in completion menu
-  formatting = {
-    --fields = {'abbr', 'kind', 'menu'},
-    format = require("lspkind").cmp_format({
-      mode = "symbol",       -- show only symbol annotations
-      maxwidth = 50,         -- prevent the popup from showing more than provided characters
-      ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
-    }),
-  },
-  --formatting = cmp_format,
+		-- Scroll up and down in the completion documentation
+		["<C-u>"] = cmp.mapping.scroll_docs(-4),
+		["<C-d>"] = cmp.mapping.scroll_docs(4),
+		-- Super Tab
+		["<Tab>"] = cmp_action.luasnip_supertab(),
+		["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
+	}),
+	--- (Optional) Show source name in completion menu
+	formatting = {
+		--fields = {'abbr', 'kind', 'menu'},
+		format = require("lspkind").cmp_format({
+			mode = "symbol", -- show only symbol annotations
+			maxwidth = 50, -- prevent the popup from showing more than provided characters
+			ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+		}),
+	},
+	--formatting = cmp_format,
 })
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype("gitcommit", {
-  sources = cmp.config.sources({
-    { name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
-  }, {
-    { name = "buffer" },
-  }),
+	sources = cmp.config.sources({
+		{ name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+	}, {
+		{ name = "buffer" },
+	}),
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline({ "/", "?" }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = "buffer" },
-  },
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(":", {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = "path" },
-  }, {
-    { name = "cmdline" },
-  }),
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
 })
 
 -- Default options:
 require("catppuccin").setup({
-  flavour = "mocha", -- latte, frappe, macchiato, mocha
-  background = {     -- :h background
-    light = "latte",
-    dark = "mocha",
-  },
+	flavour = "mocha", -- latte, frappe, macchiato, mocha
+	background = { -- :h background
+		light = "latte",
+		dark = "mocha",
+	},
 })
 
 require("kanagawa").setup({
-  theme = "wave",    -- Load "wave" theme when 'background' option is not set
-  background = {     -- map the value of 'background' option to a theme
-    dark = "dragon", -- try "dragon" !
-    light = "lotus",
-  },
+	theme = "wave", -- Load "wave" theme when 'background' option is not set
+	background = { -- map the value of 'background' option to a theme
+		dark = "dragon", -- try "dragon" !
+		light = "lotus",
+	},
 })
 
 require("Comment").setup({
-  toggler = {
-    ---Line-comment toggle keymap
-    line = "<C-`>",
-    ---Block-comment toggle keymap
-    --block = 'gbc',
-  },
-  opleader = {
-    ---Line-comment keymap
-    line = "<C-`>",
-    ---Block-comment keymap
-    --block = 'gb',
-  },
+	toggler = {
+		---Line-comment toggle keymap
+		line = "<C-`>",
+		---Block-comment toggle keymap
+		--block = 'gbc',
+	},
+	opleader = {
+		---Line-comment keymap
+		line = "<C-`>",
+		---Block-comment keymap
+		--block = 'gb',
+	},
 })
 
 require("conform").setup({
-  formatters_by_ft = {
-    --lua = { "stylua" },
-    --python = { "isort", "black" },
-    --javascript = { { "prettierd", "prettier" } },
-  },
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "black" },
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
+})
+
+require("lint").linters_by_ft = {
+	python = { "pydocstyle" },
+}
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	callback = function()
+		require("lint").try_lint()
+	end,
 })
 
 -- setup must be called before loading
